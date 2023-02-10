fdf989383c79e84dbc937cff70c7c1eaa7041ff5
Rafael masselli <89049153+rafaelmasselli@users.noreply.github.com>
2023-02-10 14:50:20 -0300

    fix: erro variavel de ambiente do shell script

--- service-create.sh
+++ service-create.sh
@@ -7,34 +7,33 @@ read Alternative
 lowerstAlternative=$(echo $Alternative | tr '[:upper:]' '[:lower:]')
 
 if [ $lowerstAlternative = "s" ]; then
-    echo "Sucesso"
+cd /etc/systemd/system/
+echo "Criando o arquivo incialize-fans.service, no /etc/systemd/system"
 
-    cd /etc/systemd/system
-    echo "Criando o arquivo incialize-fans.service, no /etc/systemd/system"
+echo "[Unit]
+Description="script to run graphics card fans"
+After=network.target
 
-    echo "[Unit]
-    Description="Script para rodar as fans da placa de video toda vez que ligar o sistema"
-    After=network.target
+[Service]
+Type=simple
+ExecStart=/bin/bash /Start-AMD-graphics-card-fans-ShellScript/inicializefans.sh
+TimeoutStartSec=0
 
-    [Service]
-    Type=simple
-    ExecStart=/bin/bash /home/rafaelmasselli/Start-AMD-graphics-card-fans-ShellScript/inicializefans.sh 
-    TimeoutStartSec=0
+[Install]
+WantedBy=default.target
+"> inicialize-fans.service
 
-    [Install]
-    WantedBy=default.target" > inicialize-fans.service
+echo "Habilitando o arquivo inicialize-fans.service"
 
-    echo "Habilitando o arquivo inicialize-fans.service"
-    systemctl enable inicialize-fans.service
+systemctl daemon-reload
+systemctl enable inicialize-fans.service
 
-    echo "Status da inicializacao das fans"
-    systemctl status inicialize-fans.service
+echo "O sistema ira reiniciar a 10 segundos"
+start=$(date +%s)
+sleep 10
+end=$(date +%s)
+reboot
 
-    echo "O sistema ira reiniciar a 10 segundos"
-    start=$(date +%s)
-    sleep 10
-    end=$(date +%s)
-    reboot
 else
-    echo "👾"
+     echo "👾"
 fi
