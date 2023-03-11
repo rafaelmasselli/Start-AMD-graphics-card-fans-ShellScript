082b9762418b113c6ab23ddda747c3b3f5791b3f
Rafael masselli <89049153+rafaelmasselli@users.noreply.github.com>
2023-03-11 14:45:50 -0300

    Update inicializefans.sh

--- inicializefans.sh
+++ inicializefans.sh
@@ -2,6 +2,6 @@
 
 echo ´Executando fans da placa de video com 36% de potência´
 
-cd /start-AMD-graphics-card-fans-ShellScript/
+cd /start-amd-graphics-card-fans/
 chmod +x amdgpu-pro-fans.sh
 sudo ./amdgpu-pro-fans.sh -s 36
